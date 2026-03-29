0927dd500770287facf6a8152ff3df27c6b2d2f8
{bogdan} <{bogdanmihajlenko7@gmail.com}>
2026-03-30 02:02:32 +0300

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,36 @@
-'use strict';
+// 'use strict';
 
-const asideBar = document.querySelector('.header__aside')
-const burgerOpen = document.querySelectorAll('.icon--burger, .icon--close' )
-const topBar = document.querySelector('.top-bar__menu')
-const heroSection = document.querySelector('.hero')
+// const asideBar = document.querySelector('.header__aside')
+// const burgerOpen = document.querySelectorAll('.icon--burger, .icon--close' )
+// const topBar = document.querySelector('.top-bar__menu')
+// const heroSection = document.querySelector('.hero')
+// const btn = document.getElementById('lox')
 
+// let currentState = false
 
-let currentState = false
+// function changeState(state) {
+// return !state
+// }
 
-function changeState(state) {
-return !state
-}
+// function addClass() {
 
-function addClass() {
+//   if (currentState === true) {
+//     asideBar.classList.add('header__aside--active') 
+//     topBar.classList.add('top-bar--hidden')
+//     heroSection.classList.add('hero--hidden')
+//   }
+//   else {
+//     asideBar.classList.remove('header__aside--active')
+//     topBar.classList.remove('top-bar--hidden')
+//     heroSection.classList.remove('hero--hidden')
+//   }
+// }
 
-  if (currentState === true) {
-    asideBar.classList.add('header__aside--active') 
-    topBar.classList.add('top-bar--hidden')
-    heroSection.classList.add('hero--hidden')
-  }
-  else {
-    asideBar.classList.remove('header__aside--active')
-    topBar.classList.remove('top-bar--hidden')
-    heroSection.classList.remove('hero--hidden')
-  }
-}
+// burgerOpen.forEach(btn => {
+//      btn.addEventListener('click', () => {
+//   currentState = changeState(currentState)
+//   addClass()
+// })
+// })
 
-burgerOpen.forEach(btn => {
-     btn.addEventListener('click', () => {
-  currentState = changeState(currentState)
-  addClass()
-})
-})
-
+// btn.innerHTML =
